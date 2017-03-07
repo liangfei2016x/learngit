1315f16d2c6392aad4e206c49602a54fa3f285d5
fei.liang <fei.liang@liangfei.gticom.cn>
2017-03-07 17:58:23 +0800

    add test

---
+++ Demo/src/com/liang/test/Test1.java
@@ -0,0 +1,20 @@
+package com.liang.test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//准确获取时间 年月日
+		LocalDate date=LocalDate.now();
+		LocalTime time=LocalTime.now();
+		System.out.println(date+" "+time);
+		List list=new ArrayList<>();
+		list.add("");
+	}
+
+}
